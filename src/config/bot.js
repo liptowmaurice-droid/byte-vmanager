926823b2c80b9c72813ee014939b3c1011471b9b
liptowmaurice-droid <liptowmaurice@gmail.com>
2026-06-05 17:33:25 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Byte-V",
+        name: "Byte-V Discord",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
